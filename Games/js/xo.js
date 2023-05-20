fffbf870ebca1215d2eb4ce89373f4a29df27380
Jostin C <91860458+joshtin2505@users.noreply.github.com>
2023-05-20 12:33:21 -0500

    Add files via upload

--- Games/js/xo.js
+++ Games/js/xo.js
@@ -1,48 +1,229 @@
-const celdas =document.querySelector('.celda')
+const celdas = document.querySelector('.celda')
+const turnoSDiv = document.getElementById('turn')
 
-const celda1 = document.getElementsByClassName('onec');
-const x1 = document.getElementsByClassName('ox');
-const c1 = document.getElementsByClassName('oc');
+const celda1 = document.querySelector('.onec');
+const x1 = document.querySelector('.ox');
+const c1 = document.querySelector('.oc');
 
-const celda2 = document.getElementsByClassName('twoc');
-const x2 = document.getElementsByClassName('sx');
-const c2 = document.getElementsByClassName('sc');
+const celda2 = document.querySelector('.twoc');
+const x2 = document.querySelector('.sx');
+const c2 = document.querySelector('.sc');
 
-const celda3 = document.getElementsByClassName('trec');
-const x3 = document.getElementsByClassName('tx');
-const c3 = document.getElementsByClassName('tc');
+const celda3 = document.querySelector('.trec');
+const x3 = document.querySelector('.tx');
+const c3 = document.querySelector('.tc');
 
-const celda4 = document.getElementsByClassName('fouc');
-const x4 = document.getElementsByClassName('fx');
-const c4 = document.getElementsByClassName('fc');
+const celda4 = document.querySelector('.fouc');
+const x4 = document.querySelector('.fx');
+const c4 = document.querySelector('.fc');
 
-const celda5 = document.getElementsByClassName('fivc');
-const x5 = document.getElementsByClassName('fix');
-const c5 = document.getElementsByClassName('fic');
+const celda5 = document.querySelector('.fivc');
+const x5 = document.querySelector('.fix');
+const c5 = document.querySelector('.fic');
 
-const celda6 = document.getElementsByClassName('sixc');
-const x6 = document.getElementsByClassName('sx');
-const c6 = document.getElementsByClassName('sc');
+const celda6 = document.querySelector('.sixc');
+const x6 = document.querySelector('.six');
+const c6 = document.querySelector('.sic');
 
-const celda7 = document.getElementsByClassName('sevc');
-const x7 = document.getElementsByClassName('sex');
-const c7 = document.getElementsByClassName('sec');
+const celda7 = document.querySelector('.sevc');
+const x7 = document.querySelector('.sex');
+const c7 = document.querySelector('.sec');
 
-const celda8 = document.getElementsByClassName('eigc');
-const x8 = document.getElementsByClassName('ex');
-const c8 = document.getElementsByClassName('ec');
+const celda8 = document.querySelector('.eigc');
+const x8 = document.querySelector('.ex');
+const c8 = document.querySelector('.ec');
 
-const celda9 = document.getElementsByClassName('ninc');
-const x9 = document.getElementsByClassName('nx');
-const c9 = document.getElementsByClassName('nc');
+const celda9 = document.querySelector('.ninc');
+const x9 = document.querySelector('.nx');
+const c9 = document.querySelector('.nc');
 
-let turno = 1
+let turno = 1 // lo inicio impar
+let turnoS
 
-celdas.addEventListener('click', () => {
+// celdas.addEventListener('click', () => {
 
-    turno ++
-    console.log(turno);
+//     turno ++;
+
+//     console.log(turno);
+
+// });
+
+
+
+celda1.addEventListener('click', () => {
+
+    if ((turno % 2) == 0) {
+
+        console.log(turno);
+        turno++;
+        x1.classList.remove('ox');
+
+        celda1.style.pointerEvents = 'none';
+        turnoSDiv.innerHTML = "Turno De O"
+    }
+    else if ((turno % 2) != 0) {
+        console.log(turno);
+        turno++;
+        c1.classList.remove('oc');
+        
+        celda1.style.pointerEvents = 'none';
+        turnoSDiv.innerHTML = "Turno De X"
+    } 
+    
+});
+celda2.addEventListener('click', () => {
+    
+    if ((turno % 2) == 0) {
+        
+        console.log(turno);
+        turno++;
+        x2.classList.remove('sx');
+        
+        celda2.style.pointerEvents = 'none';
+        turnoSDiv.innerHTML = "Turno De O"
+    }
+    else if ((turno % 2) != 0) {
+        console.log(turno);
+        turno++;
+        c2.classList.remove('sc');
+
+        celda2.style.pointerEvents = 'none';
+        turnoSDiv.innerHTML = "Turno De O"
+
+    }
 
 });
+celda3.addEventListener('click', () => {
 
+    if ((turno % 2) == 0) {
+
+        console.log(turno);
+        turno++;
+        x3.classList.remove('tx');
+
+        celda3.style.pointerEvents = 'none';
+    }
+    else if ((turno % 2) != 0) {
+        console.log(turno);
+        turno++;
+        c3.classList.remove('tc');
+
+        celda3.style.pointerEvents = 'none';
+    }
+
+});
+celda4.addEventListener('click', () => {
+
+    if ((turno % 2) == 0) {
+
+        console.log(turno);
+        turno++;
+        x4.classList.remove('fx');
+
+        celda4.style.pointerEvents = 'none';
+    }
+    else if ((turno % 2) != 0) {
+        console.log(turno);
+        turno++;
+        c4.classList.remove('fc');
+
+        celda4.style.pointerEvents = 'none';
+    }
+
+});
+celda5.addEventListener('click', () => {
+
+    if ((turno % 2) == 0) {
+
+        console.log(turno);
+        turno++;
+        x5.classList.remove('fix');
+
+        celda5.style.pointerEvents = 'none';
+    }
+    else if ((turno % 2) != 0) {
+        console.log(turno);
+        turno++;
+        c5.classList.remove('fic');
+
+        celda5.style.pointerEvents = 'none';
+    }
+
+});
+celda6.addEventListener('click', () => {
+
+    if ((turno % 2) == 0) {
+
+        console.log(turno);
+        turno++;
+        x6.classList.remove('six');
+
+        celda6.style.pointerEvents = 'none';
+    }
+    else if ((turno % 2) != 0) {
+        console.log(turno);
+        turno++;
+        c6.classList.remove('sic');
+
+        celda6.style.pointerEvents = 'none';
+    }
+
+});
+celda7.addEventListener('click', () => {
+
+    if ((turno % 2) == 0) {
+
+        console.log(turno);
+        turno++;
+        x7.classList.remove('sex');
+
+        celda7.style.pointerEvents = 'none';
+    }
+    else if ((turno % 2) != 0) {
+        console.log(turno);
+        turno++;
+        c7.classList.remove('sec');
+
+        celda7.style.pointerEvents = 'none';
+    }
+
+});
+celda8.addEventListener('click', () => {
+
+    if ((turno % 2) == 0) {
+
+        console.log(turno);
+        turno++;
+        x8.classList.remove('ex');
+
+        celda8.style.pointerEvents = 'none';
+    }
+    else if ((turno % 2) != 0) {
+        console.log(turno);
+        turno++;
+        c8.classList.remove('ec');
+
+        celda8.style.pointerEvents = 'none';
+    }
+
+});
+celda9.addEventListener('click', () => {
+
+    if ((turno % 2) == 0) {
+
+        console.log(turno);
+        turno++;
+        x9.classList.remove('nx');
+
+        celda9.style.pointerEvents = 'none';
+    }
+    else if ((turno % 2) != 0) {
+        console.log(turno);
+        turno++;
+        c9.classList.remove('nc');
+
+        celda9.style.pointerEvents = 'none';
+    }
+
+});
 
